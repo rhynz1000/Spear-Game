66ab1cc9cc3750adcad6ae9146416121a552e666
rhynz <rhynz@hotmail.co.nz>
2019-10-10 19:38:25 +1300

    reset

--- SpearGame-Debug/Graphics Proj/CTile.cpp
+++
@@ -1,8 +0,0 @@
-#include "CTile.h"
-
-void CTile::Initalise(CCamera * newCamera, float sizeH, float sizeW, float initalX, float initalY, GLuint prog, GLuint tex)
-{
-	CQuad::Initalise(newCamera, sizeH, sizeW, initalX, initalY, prog, tex);
-
-	boxCollider.initalise(-0.5, 0.5, 0.5, -0.5f, this);
-}
--- SpearGame-Debug/Graphics Proj/Quad.cpp
+++
@@ -1,7 +0,0 @@
-#include "Quad.h"
-
-void CQuad::Initalise(CCamera* newCamera, float sizeH, float sizeW, float initalX, float initalY, GLuint prog, GLuint tex)
-{
-	//initalizing quad with shapes arrays
-	C2DShape::Initalise(newCamera, sizeH, sizeW, initalX, initalY, prog, tex, vertices, indices, sizeof(vertices), sizeof(indices));
-}
